1ed19b61f6484b0ac3a463a6be57156421573890
Silveryard <sebastian-bischlager@web.de>
2017-05-12 14:19:56 +0200

    Removed import

--- APFCreator/src/main/java/de/silveryard/apfcreator/Creator.java
+++ APFCreator/src/main/java/de/silveryard/apfcreator/Creator.java
@@ -12,7 +12,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static de.silveryard.apfcreator.Assert.*;
 
