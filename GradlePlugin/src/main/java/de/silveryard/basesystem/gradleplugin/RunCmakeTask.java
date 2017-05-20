b7bf60451863aa25eb5d0dd8a247dafbb535368c
Sebastian Bischlager <sebastian-bischlager@web.de>
2017-05-20 16:33:59 +0200

    Made Cmake run on windows

--- GradlePlugin/src/main/java/de/silveryard/basesystem/gradleplugin/RunCmakeTask.java
+++ GradlePlugin/src/main/java/de/silveryard/basesystem/gradleplugin/RunCmakeTask.java
@@ -1,5 +1,7 @@
 package de.silveryard.basesystem.gradleplugin;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.InputDirectory;
 import org.gradle.api.tasks.InputFile;
@@ -32,7 +34,36 @@ public class RunCmakeTask extends DefaultTask {
 
     @TaskAction
     public void invoke() throws Exception {
-        Helper.execute("cmake ../.. -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=" + getBinDirectory().getAbsolutePath() + " -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + getBinDirectory().getAbsolutePath(), getCmakeDirectory());
-        Helper.execute("cmake --build . --target all", getCmakeDirectory());
+        String commandLine = "cmake ../.. -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=" + getBinDirectory().getAbsolutePath() + " -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + getBinDirectory().getAbsolutePath();
+
+        if(SystemUtils.IS_OS_WINDOWS){
+            commandLine += " -G \"Visual Studio 15 2017 Win64\"";
+        }
+
+        Helper.execute(commandLine, getCmakeDirectory());
+
+        commandLine = "cmake --build . --target ";
+
+        if(SystemUtils.IS_OS_WINDOWS){
+            commandLine += "ALL_BUILD";
+        }else{
+            commandLine += "all";
+        }
+
+        Helper.execute(commandLine, getCmakeDirectory());
+
+        if(SystemUtils.IS_OS_WINDOWS){
+            File debugDirectory = new File(getBinDirectory(), "Debug");
+            File releaseDirectory = new File(getBinDirectory(), "Release");
+
+            if(FileUtils.directoryContains(getBinDirectory(), debugDirectory)){
+                FileUtils.copyDirectory(debugDirectory, getBinDirectory());
+                FileUtils.deleteDirectory(debugDirectory);
+            }
+            if(FileUtils.directoryContains(getBinDirectory(), releaseDirectory)){
+                FileUtils.copyDirectory(releaseDirectory, getBinDirectory());
+                FileUtils.deleteDirectory(releaseDirectory);
+            }
+        }
     }
 }
